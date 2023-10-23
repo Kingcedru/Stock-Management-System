313852ac2d22d171fa8d18f51bda8c9ab775850e
Kingcedru <byishimocedrick@gmail.com>
2023-10-23 17:41:36 +0200

    logn form

--- index.php
+++ index.php
@@ -8,9 +8,16 @@
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline bg-red-400">
-        Hello world!
-    </h1>
+    <section class='flex flex-col items-center gap-20 h-screen'>
+        <h1 class="text-3xl font-bold">
+            Stock Management Stock
+        </h1>
+        <form class=' flex flex-col pt-10 bg-indigo-600 py-10 px-10 w-4/6 h-4/6 rounded-2xl'>
+            <label>Username</label><input type=' text' placeholder='Enter username' class='border-2'>
+            <label>Password</label><input type=' password' placeholder='Enter password'>
+            <input type='submit' name='login' value='Log in' class='border-2'>
+        </form>
+    </section>
 </body>
 
 </html>
